0b5105d652374b3ccc42b81d810633f215ddc72c
Vincent Lesierse <vincent@lesierse.com>
2018-12-14 10:28:33 +0100

    Toevoegen oplossing Java: Beginner

---
+++ java-beginner/README.md
@@ -0,0 +1,4 @@
+# Java: Beginner
+
+## Configuratie
+
---
+++ java-beginner/oplossing.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        boolean user_right = true;
+        while (user_right) {
+            System.out.println("Raadt kop (k) of munt (m)!");
+
+            Scanner user_input = new Scanner(System.in);
+            String user_guess = user_input.next();
+
+            System.out.println("Je hebt geraden: " + user_guess);
+
+            Random coin = new Random();
+            int toss = coin.nextInt(2);
+            if (toss == 1) {
+                System.out.println("De munt is op kop geland");
+            }
+            if (toss == 0) {
+                System.out.println("De munt is op munt geland");
+            }
+
+            int guess_number = 0;
+            if (user_guess.equals("k")) {
+                guess_number = 1;
+            }
+            if (user_guess.equals("m")) {
+                guess_number = 0;
+            }
+
+            if (toss == guess_number) {
+                System.out.println("Je hebt juist geraden!");
+            }
+            if (toss != guess_number) {
+                System.out.println("Je hebt verkeerd geraden! Game over!");
+                user_right = false;
+            }
+        }
+    }
+}
